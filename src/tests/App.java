0db591e3255b16c51e1856abb3bd1fe94b2e85cb
OctavioPv <oc2003PeVz99@gmail.com>
2022-05-08 22:51:20 -0500

    Menu Base

--- src/tests/App.java
+++ src/tests/App.java
@@ -11,69 +11,20 @@ public class App {
         SimpleDateFormat df = new SimpleDateFormat("dd 'de' MMMM, yyyy");
         System.out.println(df.format(fecha));
 
-        int opcion = 0, opcion2 = 0, opcion3= 0;
+        int opcion = 0;
         do{
             opcion = mostrarMenu();
             switch(opcion){
-                case 11:
+                case 1:
 
                     break;
-                case 12:
+                case 2:
                     
                     break;
-                case 13:
+                case 3:
 
                     break;
-                case 14:
-                    
-                    break;
-                case 21:
-                    
-                    break;
-                case 22:
-                    
-                    break;
-                case 23:
-                   
-                    break;
-                case 24:
-                    
-                    break;
-                case 31:
-                    opcion2 = mostrarMenu();
-                    switch(opcion2){
-                        case 1:
-                            
-                        case 2:
-                            
-                        case 3:
-                            
-                    }
-                    break;
-                case 32:
-                    opcion3 = mostrarMenu();
-                    switch(opcion3){
-                        case 1:
-                            
-                            break;
-                        case 2:
-                            
-                            break;
-                        case 3:
-                            
-                            break;
-                    }
-                    break;
-                case 33:
-                    
-                    break;
-                case 34:
-                    
-                    break;
-                case 41:
-                    
-                    break;
-                case 42:
+                case 4:
                     
                     break;
                 case 0: return;
@@ -85,17 +36,16 @@ public class App {
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
         System.out.println();
-        System.out.println("<<< (1). Capturar Restaurante   (2). Capturar Menu   (3). Capturar Propietarios   (0). Salir  >>>  ");
+        System.out.println("< (1). MindBox   (2). TeachBox   (3). Library   (4). Administration   (0). Salir  >");
         System.out.println();
         System.out.print("Seleccione una opcion: ");
         opcion = sc.nextInt();
 
-        if (opcion == 0)
+        if (opcion == 0) {
             return opcion;
-
-
+        }
         return opcion;
     }
 
-    }
+
 }
